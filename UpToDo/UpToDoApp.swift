82064e2905430c4ec6449c6ae42c195151bcfebf
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-11-03 11:06:56 +0100

    Initial Commit

---
+++ UpToDo/UpToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpToDoApp.swift
+//  UpToDo
+//
+//  Created by Amaechi Chukwu on 03/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct UpToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
